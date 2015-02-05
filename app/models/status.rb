3b39ce7fb54afe12cca815db2303fb8e9406c6b5
stirkac <sebastjan.stojnsek@gmail.com>
2015-02-05 20:40:02 +0100

    Scheduled

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,6 +1,6 @@
 class Status < ActiveRecord::Base
 	def initialize
-		current_status = DhtSensorReader.get_dht_sensor_reading
+		current_status = DhtSensorReader.get_dht_sensor_reading Rails.root
 		self[:temperature] = current_status['temperature']
 		self[:humidity] = current_status['humidity']
 	end
--- config/schedule.rb
+++ config/schedule.rb
@@ -7,11 +7,9 @@
 #
 # set :output, "/path/to/my/cron_log.log"
 #
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
+ every 1.minutes do
+   runner "Status.initialize"
+ end
 #
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
