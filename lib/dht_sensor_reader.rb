0685ee902739f0b6847fe9684a9b58d2418eee90
stirkac <sebastjan.stojnsek@gmail.com>
2015-02-05 18:07:46 +0100

    Fixed location

--- lib/dht_sensor_reader.rb
+++ lib/dht_sensor_reader.rb
@@ -1,8 +1,7 @@
 require 'json'
-
 module DhtSensorReader
-  def get_dht_sensor_reading
-  	result = %x( rvmsudo ruby status.rb )
+  def get_dht_sensor_reading rails_root
+  	result = %x( rvmsudo ruby #{rails_root}/lib/status.rb )
   	return JSON.parse(result.split("---data---")[-1])
   end
 end
