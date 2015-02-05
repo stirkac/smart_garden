981805fc2886aead8ac23a2c4482b85342297278
stirkac <sebastjan.stojnsek@gmail.com>
2015-02-05 18:22:21 +0100

    Changed string

--- lib/status.rb
+++ lib/status.rb
@@ -1,4 +1,4 @@
 require "dht-sensor-ffi"
 val = DhtSensor.read(4, 22)
 puts '---data---'
-puts '{ "temperature": #{val.temp}, "humidity": #{val.humidity}}'
+puts '{ "temperature": '+val.temp+', "humidity": '+val.humidity+'}'
