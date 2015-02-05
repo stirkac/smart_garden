412d1ba0e2ed72165b70944942b231e28117e306
stirkac <sebastjan.stojnsek@gmail.com>
2015-02-05 20:46:19 +0100

    Fixed naming

--- lib/dht_sensor_reader.rb
+++ lib/dht_sensor_reader.rb
@@ -1,7 +1,7 @@
 require 'json'
 module DhtSensorReader
   def self.get_dht_sensor_reading rails_root
-  	result = %x( rvmsudo ruby #{rails_root}/lib/status.rb )
+  	result = %x( rvmsudo ruby #{rails_root}/lib/reader.rb )
   	return JSON.parse(result.split("---data---")[-1])
   end
 end
