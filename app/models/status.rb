097f94a0d597c35ddff497b1d0dad8e0ab3838bc
stirkac <sebastjan.stojnsek@gmail.com>
2015-02-05 22:32:10 +0100

    Initial state fix

--- app/models/status.rb
+++ app/models/status.rb
@@ -8,8 +8,8 @@ class Status < ActiveRecord::Base
 	def make_reading
 		current_status = DhtSensorReader.get_dht_sensor_reading Rails.root
 		last_status = Status.order(:created_at).last
-		delta_t = last_status.temperature - current_status['temperature'].to_f
-		delta_h = last_status.humidity - current_status['humidity'].to_f
+		delta_t = last_status.temperature - current_status['temperature'].to_f rescue 1
+		delta_h = last_status.humidity - current_status['humidity'].to_f rescue 1
 
 		if(delta_t.abs > 0.5 || delta_h.abs > 0.5)
 			self[:temperature] = current_status['temperature']
