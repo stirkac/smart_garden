fd61e6d5def207510513a919c803613ed2269ab9
stirkac <sebastjan.stojnsek@gmail.com>
2015-02-05 19:50:59 +0000

    Renamed method

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,5 @@
 class Status < ActiveRecord::Base
-	def initialize
+	def make_reading
 		current_status = DhtSensorReader.get_dht_sensor_reading Rails.root
 		self[:temperature] = current_status['temperature']
 		self[:humidity] = current_status['humidity']
