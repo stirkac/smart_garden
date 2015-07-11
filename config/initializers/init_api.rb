be67f5c9f24d2ceb2aecb96f9eff906244b6b59e
stirkac <sebastjan.stojnsek@gmail.com>
2015-07-11 18:48:10 +0200

    Error handling improved

--- config/initializers/init_api.rb
+++ config/initializers/init_api.rb
@@ -1,11 +1,12 @@
 SmartGarden::Application.config.after_initialize do
   puts "Setting up API..."
+  address = ENV['MOTHERSHIP_URL']+"/register_device"
   begin
-	  device_status = HTTParty.post(ENV['MOTHERSHIP_URL']+"/register_device")
+	  device_status = HTTParty.post(address)
 		response = case device_status.code
-			when 304 then "remote: done: Sensor already registered!"
-			when 200 then "remote: done: Sensor added and registered"
-			else "remote: fail: Something went wrong (#{device_status.code})"
+			when 304 then "#{address}: done: Sensor already registered!"
+			when 200 then "#{address}: done: Sensor added and registered"
+			else "#{address}: fail: Something went wrong (#{device_status.code})"
 		end
 		puts response
 	rescue
