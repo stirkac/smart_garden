b25bd79c9e0ef4c6b926ce2a3658b6f776163aba
stirkac <sebastjan.stojnsek@gmail.com>
2015-08-11 15:13:06 +0200

    Routes format fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
-  scope format: true, defaults: { format: 'json' } do
-    get "/current", controller: :statuses, action: :current
-    get "/statuses", controller: :statuses, action: :index
-  end
-
+  get "/current", controller: :statuses, action: :current
+  get "/statuses", controller: :statuses, action: :index
 end
